59a601890d16cc0efeb2e53adc0b9388d0ec07bd
Santi3699 <santiagopicon19992@gmail.com>
2025-04-30 17:42:56 -0500

    Create listas300425.py

---
+++ listas300425.py
@@ -0,0 +1,25 @@
+#lista = [1, 2, 3]
+#print(lista)
+#lista.append(4)
+#print(lista)
+#lista.extend([4, 5, 6])
+#print(lista)
+#lista.insert(1, 'nuevo')
+#print(lista)
+#lista = [1, 2, 3, 2]
+#lista.remove(2)
+#print(lista)
+#lista = [1, 2, 3]
+#elemento = lista.pop()
+#print(lista)
+#lista = [3, 1, 2]
+#lista.sort()  # lista ahora es [1, 2, 3]
+#print(lista)
+
+#tupla = (1, 2, 3)
+#print(tupla[0])  # Accede al primer elemento
+
+#diccionario = {"nombre": "Juan", "edad": 30}
+#diccionario["profesión"] = "Ingeniero"
+#print(diccionario)
+
