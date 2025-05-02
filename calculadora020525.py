575e5cb88316207b7e19d10a85b13fc8d5feef0c
Santi3699 <santiagopicon19992@gmail.com>
2025-05-02 17:31:10 -0500

    Create calculadora020525.py

---
+++ calculadora020525.py
@@ -0,0 +1,25 @@
+#def sumar():
+
+#valor1 = float(input("ingrese numero 1"))
+#valor2 = float(input("ingrese numero 2"))
+#resultado = numero1 + numero2
+#print(resultado)
+
+#sumar()
+
+#def sumar(valor1, valor2):
+#    return valor1 + valor2
+
+#resultado = sumar(valor1,valor2)
+#print(resultado)  # Imprime 8
+    
+#suma = lambda x, y: x / y
+
+#resultado = suma(48, 4)
+#print(resultado)  # Imprime 11
+
+def operacion ()
+valor1 = float(input(ingrese grados celsuis))
+valor2 = float(input(ingrese grados Farenhe))
+resultado = x,y: x=(y*1.8)+32
+print(resultado) 
