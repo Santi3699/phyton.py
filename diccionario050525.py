51eb04314c8481140bf4836dc83359599ae0891c
Santi3699 <santiagopicon19992@gmail.com>
2025-05-05 17:41:27 -0500

    Update diccionario050525.py

--- diccionario050525.py
+++ diccionario050525.py
@@ -57,3 +57,26 @@
 #        break
 #    respuesta = obtener_respuesta(mensaje)
 #    print(respuesta)
+
+#pip install google-generativeai- Se debe cargar la libreria en el terminal para poder anclarlo 
+import google.generativeai as genai
+
+GEMINI_API_KEY = "su_keyAIzaSyAdYUmLipLDUs49S8GoJQ7CoyvNJYKEYPA"
+genai.configure(api_key=GEMINI_API_KEY)
+modelo = genai.GenerativeModel("gemini-2.0-flash")
+
+def obtener_respuesta(mensaje):
+    try:
+        respuesta = modelo.generate_content(mensaje)
+        return respuesta.text.strip()
+    except Exception as e:
+        print("Error al generar respuesta:", e)
+        return "Lo siento, hubo un problema al procesar tu pregunta."
+    
+print("hola soy un chabot de soporte al cliente")
+while True:
+    mensaje = input("ingrese su consulta")
+    if mensaje.lower() == "salir":
+        break
+    respuesta = obtener_respuesta(mensaje)
+    print(respuesta)
