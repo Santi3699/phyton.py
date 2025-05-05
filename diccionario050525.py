547139bca2800012f6d80168eacb9ad57a581abc
Santi3699 <santiagopicon19992@gmail.com>
2025-05-05 17:21:22 -0500

    Create diccionario050525.py

---
+++ diccionario050525.py
@@ -0,0 +1,59 @@
+#persona = {
+#    "nombre": "Carlos",
+#    "edad": 30,
+#    "ciudad": "Bogotá"
+#}
+
+#print(persona["nombre"])
+#print(persona["edad"])
+#print(persona["ciudad"])
+
+#persona["profesión"] = "Ingeniero"
+#persona["edad"] = 31
+
+#print(persona)
+
+#clave = input("Ingrese una clave: ")
+
+#if clave in persona:
+#    print(f"{clave} existe y su valor es: {persona[clave]}")
+#else:
+#    print(f"{clave} no existe en el diccionario.")
+
+#del persona["ciudad"]
+#print(persona)
+
+#respuestas = {
+#    "hola": "hola como estas",
+#    "que cursos tiene disponibles": "cursos de ai, programacion",
+#    "donde estan ubicados": "estamos en la cra 34 #98",
+#    "como me puedo inscribir": "para incribirme visitar la pagina de talento tech",
+#    "chao": "hasta luego fue un gusto ayudarte"
+#}
+#del persona["hola"]
+#print(persona)
+
+#clave = input ("ingrese una clave: ")
+
+
+#def obtener_respuesta(mensaje)
+#if clave in persona:
+#    print(f"{clave} existe y su valor es: {persona[clave]}")
+#else:
+#    print(f"{clave} no existe en el diccionario.")
+
+#print(len(persona))
+
+#def obtener_respuesta(mensaje):
+#    if mensaje in respuestas:
+#        return respuestas[mensaje]
+#    else:
+#        return"no entiendo tu pregunta"
+
+#print("hola soy un chatbot de soporte al cliente")
+#while True:
+#    mensaje = input("ingrese su consulta")
+#    if mensaje.lower()=="salir":
+#        break
+#    respuesta = obtener_respuesta(mensaje)
+#    print(respuesta)
